47455f20daf2d6a97b481fe5c43b60ac5340b0d9
ThibaultLycee <thib.durand@free.fr>
2022-10-17 22:40:23 +0200

    Added a way to display a text buffer, and load a file to read such buffer

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-use terminal_size::{Width, Height, terminal_size};
+use std::env;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::{Write, stdout, stdin, Stdin, Stdout};
+use std::io::prelude::*;
+use std::process::Command;
+
+use termion::color;
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::{RawTerminal, IntoRawMode};
-use termion::color;
-use std::io::{Write, stdout, stdin, Stdin, Stdout};
-use std::env;
-use std::process::Command;
+
+use terminal_size::{Width, Height, terminal_size};
 
 // Differents mode for reading / editing
 enum ViewModes
@@ -15,9 +20,8 @@ enum ViewModes
     RUNNING_CMD,
 }
 
-#[derive(Debug)]
 enum Instr {
-    LOAD_FILE,
+    LOAD_FILE(String),
     SAVE_FILE,
     QUIT,
     RUN(String),
@@ -49,7 +53,7 @@ impl EscChr {
 
 fn clearSeparatorLine(term_s : &mut Size2) {
     let mut empty_line = String::with_capacity(term_s.x.into());
-    for i in 1..term_s.x { empty_line.push(' '); }
+    for i in 0..term_s.x { empty_line.push(' '); }
     print!("{}{}{}{}",
            termion::cursor::Goto(1, term_s.y-1),
            color::Bg(color::LightBlack),
@@ -62,18 +66,18 @@ fn setupScreen(stdout : &mut RawTerminal<Stdout>, term_s : &mut Size2) {
            EscChr::CLR,
            termion::cursor::Goto(1, 1)).unwrap();
     clearSeparatorLine(term_s);
+    for i in 1..term_s.y-1 {
+        print!("{}{}~{}",
+               color::Fg(color::Blue),
+               termion::cursor::Goto(1, i),
+               color::Fg(color::Reset));
+    }
 }
 
 fn initScreen(stdout : &mut RawTerminal<Stdout>) -> Option<Size2> {
     // Tries to get the terminal size
     if let Some((Width(w), Height(h))) = terminal_size() {
         // Prints a '~' at the begining of each line
-        for i in 1..h-1 {
-            print!("{}{}~{}",
-                   color::Fg(color::Blue),
-                   termion::cursor::Goto(1, i),
-                   color::Fg(color::Reset));
-        }
         setupScreen(stdout, &mut Size2::new(w, h));
         print!("{}", termion::cursor::Goto(1, h));
         // Flushes the output
@@ -84,6 +88,41 @@ fn initScreen(stdout : &mut RawTerminal<Stdout>) -> Option<Size2> {
         println!("Unable to get term size, exiting tbe");
         None
     }
+}
+
+fn loadFile(path : &mut String, buff : &mut Vec<String>) -> std::io::Result<()> {
+    let file = File::open(path)?;
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents)?;
+
+    for line in contents.split('\n')
+    {
+        buff.push(String::from(line));
+    }
+
+    Ok(())
+}
+
+fn updateBuffer(buffer : &mut Vec<String>, coord : &mut Size2, offset : &mut Size2, char : &str) {
+    //let off_y = (offset.y + coord.y) as u16;
+    buffer[(offset.y + coord.y) as usize].replace_range(coord.x as usize..(coord.x+1) as usize, char);
+}
+
+fn showEntireBuffer(buffer : &mut Vec<String>, offset : &mut Size2, stdout : &mut RawTerminal<Stdout>, term_s : &mut Size2) {
+    let mut i = 0;
+    while i < term_s.y-2 && usize::from(i + offset.y) < buffer.len() {
+        let line_nbr : u16 = offset.y + i;
+        let line : &String = &buffer[line_nbr as usize];
+        print!("{}{}{}{} {}",
+               termion::cursor::Goto(1, i+1),
+               color::Fg(color::LightYellow),
+               line_nbr + 1,
+               color::Fg(color::Reset),
+               line);
+        i += 1;
+    }
+    stdout.flush();
 }
 
 fn remove(stri : &mut String, n : u16) {
@@ -140,6 +179,7 @@ fn insert(stdin : &mut Stdin, stdout : &mut RawTerminal<Stdout>, term_s : &mut S
 // Function that handles the COMMAND mode, manually collecting the command and handling the execution
 fn command(stdin : &mut Stdin, stdout : &mut RawTerminal<Stdout>, term_s : &mut Size2) -> Vec<Instr> {
     print!("{}", termion::cursor::Goto(1, term_s.y));
+    stdout.flush();
     let mut cmd : String = String::new();
     let mut curr_pos : Size2 = Size2::new(1, term_s.y);
     
@@ -209,7 +249,7 @@ fn runningCommand(stdin : &mut Stdin, stdout : &mut RawTerminal<Stdout>, term_s 
 
     let mut h = 2;
     for line in output_iter {
-        print!("{}{}",
+        print!("{} {}",
                termion::cursor::Goto(1, h),
                line);
         h += 1;
@@ -241,11 +281,22 @@ fn runningCommand(stdin : &mut Stdin, stdout : &mut RawTerminal<Stdout>, term_s 
 fn main() {
     // Gets the arguments given to the program through command prompt
     let args : Vec<String> = env::args().collect();
-    let mut file_path : &String = &String::from(".");
+    let mut file_path : String = String::from(".");
+    let mut buffer : Vec<String> = Vec::new();
 
     if args.len() != 0 {
-        file_path = &args[0];
+        file_path = String::from(&args[0]);
+        if loadFile(&mut file_path, &mut buffer).is_err() {
+            buffer = Vec::new();
+        }
     }
+
+    // Debug purposes only
+    buffer.push(String::from("Lorem ipsum dolor sit amet, consectetur adipiscing elit."));
+    buffer.push(String::from("Ut euismod at purus sed vehicula. In laoreet lectus ligula, sed aliquam lectus pellentesque sit amet."));
+    buffer.push(String::from("Aliquam tincidunt sit amet ipsum at semper."));
+    buffer.push(String::from("Praesent at ante vel dui sollicitudin interdum. Praesent lacus ante,"));
+    buffer.push(String::from("accumsan sed dui sed, porta viverra augue. Aliquam id. "));
 
     // Changes and sets the way stdin and stdout are handled
     let mut stdout = stdout().into_raw_mode().unwrap();
@@ -271,6 +322,8 @@ fn main() {
         // Instructions given by the different handlers for the program to execute
         let mut instrs : Vec<Instr> = Vec::new();
 
+        showEntireBuffer(&mut buffer, &mut Size2::new(0, 0), &mut stdout, &mut term_size);
+
         // Chooses which function to call depending on the current mode
         match curr_mode {
             // Gets a set of instructions back from the COMMAND mode handler
@@ -287,6 +340,12 @@ fn main() {
                 Instr::CHANGE_MODE_COMMAND      => curr_mode = ViewModes::COMMAND,
                 Instr::CHANGE_MODE_INSERT       => curr_mode = ViewModes::INSERT,
                 Instr::CHANGE_MODE_RUNNING_CMD  => curr_mode = ViewModes::RUNNING_CMD,
+                Instr::LOAD_FILE(mut path) => {
+                    buffer = Vec::new();
+                    if loadFile(&mut path, &mut buffer).is_err() {
+                        buffer = Vec::new();
+                    }
+                }
                 _ => {},
             }
         }
